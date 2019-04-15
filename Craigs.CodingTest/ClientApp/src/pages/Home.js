1e6b8d6be56c44a45ad2d08ace69b536f16dd1ce
dixylo <bixylim@gmail.com>
2019-04-15 21:02:54 +1200

    Added content and bg image to home page.

--- Craigs.CodingTest/ClientApp/src/pages/Home.js
+++ Craigs.CodingTest/ClientApp/src/pages/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render () {
     return (
-      <div>
-        Hello World
+      <div className='home'>
+        <div className='text'>
+          <p className='title'>Finding Neo</p>
+          <div className='intro'>
+            <p>Everyday, Earth has some buddies around...</p>
+            <p>They are called Near-Earth Objects, or NEOs.</p>
+            <br />
+              <button className='checkout'>Check out</button>
+            &nbsp;who's hanging out with Earth today!
+          </div>          
+        </div>
       </div>
     );
   }
