102e5a1d1b185b3d9935ddb2a1e6a3e2d8a96275
dixylo <bixylim@gmail.com>
2019-04-15 21:18:22 +1200

    Added Neos page

--- Craigs.CodingTest/ClientApp/src/pages/NeoList.js
+++ Craigs.CodingTest/ClientApp/src/pages/NeoList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import earth from '../assets/earth.png';
+import './NeoList.css';
 
 class NeoList extends Component {
   render () {
     return (
-      <div>
-        Hello Neos!
+      <div className='container'>
+        <p className='date'>2019-04-15</p>
+        <div className='earth'><img className='earthImg' alt='earch' src={earth} /></div>
       </div>
     );
   }
